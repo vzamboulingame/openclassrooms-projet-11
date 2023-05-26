f76e4d1738d15523acf206175fb764c4adbf3861
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-05-26 13:16:57 +0200

    Converted data to js object :
    - Converted logements.json to logements.js js object
    - Added logements.js to src/data
    - Imported logements.js into App.jsx
    - Added logements as data props to Home & Rental jsx
    - Removed logements.json file fom public

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Rental from "./routes/Rental";
 import NotFound from "./routes/NotFound";
+import { logements } from "./data/logements";
 import "./styles/style.scss";
 
 export default function App() {
@@ -12,9 +13,9 @@ export default function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home data={logements} />} />
         <Route path="/about" element={<About />} />
-        <Route path="/rental/:rentalId" element={<Rental />} />
+        <Route path="/rental/:rentalId" element={<Rental data={logements} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
