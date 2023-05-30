342a582e4752487c1b573a5255555e25ec6e3331
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-05-30 17:04:41 +0200

    Completed rental info section :
    - Added star svg files for rating
    - Added title, location and tags into first grid
    - Added Host name, picture and rating into second grid
    - Added styling for desktop & mobile versions of rental info section

--- src/routes/Rental.jsx
+++ src/routes/Rental.jsx
@@ -3,12 +3,39 @@ import { useParams } from "react-router-dom";
 import { RentalsContext } from "../App";
 import Carrousel from "../components/Carrousel";
 import Collapse from "../components/Collapse";
+import starRed from "../images/star-red.svg";
+import starGray from "../images/star-gray.svg";
 
 export default function Rental() {
   const rentals = useContext(RentalsContext);
   const { rentalId } = useParams();
   const rentalObj = rentals.find((rental) => rental.id === rentalId);
+  const rentalTitle = rentalObj.title;
+  const rentalLocation = rentalObj.location;
   const rentalDescription = rentalObj.description;
+  const rentalHostPicture = rentalObj.host.picture;
+  const rentalRating = Array.from(Array(5), (value, index) => {
+    const isFilled = index < rentalObj.rating;
+    const starImg = isFilled ? starRed : starGray;
+
+    return (
+      <img
+        key={index}
+        src={starImg}
+        alt="star"
+        className="rental-info-rating-star"
+      />
+    );
+  });
+
+  const rentalHostName = rentalObj.host.name
+    .split(" ")
+    .map((name, index) => <div key={index}>{name}</div>);
+  const rentalTags = rentalObj.tags.map((tag) => (
+    <span key={tag} className="rental-info-tags-item">
+      {tag}
+    </span>
+  ));
   const rentalEquipments = rentalObj.equipments.map((equipment) => (
     <div key={equipment}>{equipment}</div>
   ));
@@ -16,6 +43,23 @@ export default function Rental() {
   return (
     <main className="container main rental">
       <Carrousel data={rentalObj} />
+      <section className="rental-info">
+        <div className="rental-info-grid1">
+          <p className="rental-info-title">{rentalTitle}</p>
+          <p className="rental-info-location">{rentalLocation}</p>
+          <div className="rental-info-tags">{rentalTags}</div>
+        </div>
+        <div className="rental-info-grid2">
+          <div className="rental-info-host">
+            <div className="rental-info-host-name">{rentalHostName}</div>
+            <div
+              className="rental-info-host-picture"
+              style={{ backgroundImage: `url(${rentalHostPicture})` }}
+            ></div>
+          </div>
+          <div className="rental-info-rating">{rentalRating}</div>
+        </div>
+      </section>
       <section className="rental-collapses">
         <div className="rental-collapses-item">
           <Collapse title="Description">{rentalDescription}</Collapse>
