a971d79f609b9f3bae0928034a09a477aab25db5
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-05-30 10:46:54 +0200

    Added collapses for rental desc & equipments

--- src/routes/Rental.jsx
+++ src/routes/Rental.jsx
@@ -2,15 +2,28 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { RentalsContext } from "../App";
 import Carrousel from "../components/Carrousel";
+import Collapse from "../components/Collapse";
 
 export default function Rental() {
   const rentals = useContext(RentalsContext);
   const { rentalId } = useParams();
   const rentalObj = rentals.find((rental) => rental.id === rentalId);
+  const rentalDescription = rentalObj.description;
+  const rentalEquipments = rentalObj.equipments.map((equipment) => (
+    <div key={equipment}>{equipment}</div>
+  ));
 
   return (
     <main className="container main rental">
       <Carrousel data={rentalObj} />
+      <section className="rental-collapses">
+        <div className="rental-collapses-item">
+          <Collapse title="Description">{rentalDescription}</Collapse>
+        </div>
+        <div className="rental-collapses-item">
+          <Collapse title="Équipements">{rentalEquipments}</Collapse>
+        </div>
+      </section>
     </main>
   );
 }
